665c323f150ff9a6e386807747f737bac7ea8771
Peelson <nqh@meitu.com>
2019-07-23 14:13:56 +0800

    [feature] init project

---
+++ LogWithAspectJX4Peelson/src/main/java/com/peelson/logwithaspectjx4peelson/LogAspect.java
@@ -0,0 +1,14 @@
+package com.peelson.logwithaspectjx4peelson;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * @author peelson
+ * @Description
+ * @date 2019/07/23
+ */
+
+@Aspect
+public class LogAspect {
+
+}
